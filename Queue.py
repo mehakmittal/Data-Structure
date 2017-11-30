e3615239f720825de6aa3d5d75c4d4e195ece508
mehakmittal <mehak.mittal@credr.com>
2017-11-30 17:28:01 +0530

    file updated

--- Queue.py
+++ Queue.py
@@ -5,3 +5,31 @@ class Node(object):
 		self.data = data
 
 
+class Queue(object):
+	"""docstring for Queue"""
+	def __init__(self):
+		self.first = None
+		self.last = None
+
+	def enqueue(self, data):
+		newNode = Node(data)
+		if self.first is None and self.last is None:
+			self.first = newNode
+			self.last = self.first
+		else:
+			self.last.right = newNode
+			self.last = newNode
+
+	def dequeue(self):
+		if self.first is None:
+			return None
+		else:
+			data = self.first.data
+			next = self.first.right
+			if next is None:
+				self.first = None
+				self.last = None
+			else:
+				self.first = next
+			return data
+
